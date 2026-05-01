b2ca647a6dab3314fe986b598418db7a1f6e57db
Matigame <74415916+Matigamer64@users.noreply.github.com>
2026-05-01 17:24:43 +0200

    Redo material check

--- src/main/java/com/github/kaosreven/paxels/items/ItemGenericPaxel.java
+++ src/main/java/com/github/kaosreven/paxels/items/ItemGenericPaxel.java
@@ -1,7 +1,10 @@
 package com.github.kaosreven.paxels.items;
 
 import net.minecraft.block.Block;
+import net.modificationstation.stationapi.api.block.BlockState;
 import net.modificationstation.stationapi.api.registry.BlockRegistry;
+import net.modificationstation.stationapi.api.registry.Registry;
+import net.modificationstation.stationapi.api.registry.RegistryKey;
 import net.modificationstation.stationapi.api.tag.TagKey;
 import net.modificationstation.stationapi.api.template.item.TemplateToolItem;
 import net.modificationstation.stationapi.api.util.Identifier;
@@ -9,23 +12,36 @@ import net.modificationstation.stationapi.api.util.Identifier;
 import net.minecraft.item.ToolMaterial;
 
 public class ItemGenericPaxel extends TemplateToolItem {
-    private String MaterialTagToCheckAgainst;
+    private final ToolMaterial Tool_Material;
     public ItemGenericPaxel(Identifier identifier, ToolMaterial material) {
         super(identifier, 3, material, Block.BLOCKS);
         this.setMaxDamage(material.getDurability()*2+1);
-        switch (material) {
-            case WOOD: MaterialTagToCheckAgainst = "minecraft:needs_stone_tool"; break;
-            case STONE:
-            case GOLD:
-                MaterialTagToCheckAgainst = "minecraft:needs_iron_tool"; break;
-            case IRON: MaterialTagToCheckAgainst = "minecraft:needs_diamond_tool"; break;
-        }
+        Tool_Material = material;
     }
 
     @Override
     public boolean isSuitableFor(Block block) {
-        if (MaterialTagToCheckAgainst == null) return true;
-        else if (block.getDefaultState().isIn(TagKey.of(BlockRegistry.INSTANCE.getKey(), Identifier.of("minecraft:needs_diamond_tool")))) return false;
-        else return true;
+        RegistryKey<? extends Registry<Block>> BlockRegistryKey = BlockRegistry.INSTANCE.getKey();
+        BlockState Block_State = block.getDefaultState();
+        switch(Tool_Material) {
+            case WOOD:
+                if (
+                        Block_State.isIn(TagKey.of(BlockRegistryKey, Identifier.of("minecraft:needs_diamond_tool"))) ||
+                        Block_State.isIn(TagKey.of(BlockRegistryKey, Identifier.of("minecraft:needs_iron_tool"))) ||
+                        Block_State.isIn(TagKey.of(BlockRegistryKey, Identifier.of("minecraft:needs_stone_tool")))
+                ) return false;
+            case STONE:
+            case GOLD:
+                if (
+                        Block_State.isIn(TagKey.of(BlockRegistryKey, Identifier.of("minecraft:needs_diamond_tool"))) ||
+                        Block_State.isIn(TagKey.of(BlockRegistryKey, Identifier.of("minecraft:needs_iron_tool")))
+                ) return false;
+            case IRON:
+                if (
+                        Block_State.isIn(TagKey.of(BlockRegistryKey, Identifier.of("minecraft:needs_diamond_tool")))
+                ) return false;
+            default:
+        }
+        return true;
     }
 }
