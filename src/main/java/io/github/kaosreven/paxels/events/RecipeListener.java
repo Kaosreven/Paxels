96f882a2fe86a02926f5675bae54083743aa4e64
kaosreven <Matigame@protonmail.com>
2026-06-30 22:54:38 +0200

    Make Paxel recipes use convention tags whenever possible

--- src/main/java/io/github/kaosreven/paxels/events/RecipeListener.java
+++ src/main/java/io/github/kaosreven/paxels/events/RecipeListener.java
@@ -7,6 +7,9 @@ import net.minecraft.item.ItemStack;
 import net.modificationstation.stationapi.api.event.recipe.RecipeRegisterEvent;
 import net.modificationstation.stationapi.api.recipe.CraftingRegistry;
 import com.matthewperiut.aether.item.AetherItems;
+import net.modificationstation.stationapi.api.registry.ItemRegistry;
+import net.modificationstation.stationapi.api.tag.TagKey;
+import net.modificationstation.stationapi.api.util.Identifier;
 
 import java.util.Objects;
 
@@ -15,11 +18,11 @@ public class RecipeListener {
     public void registerRecipes(RecipeRegisterEvent event) {
         RecipeRegisterEvent.Vanilla type = RecipeRegisterEvent.Vanilla.fromType(event.recipeId);
         if (Objects.requireNonNull(type) == RecipeRegisterEvent.Vanilla.CRAFTING_SHAPED) {
-            CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Wood_Paxel, 1), "ABC", " Y ", " Y ", 'A', Item.WOODEN_AXE, 'B', Item.WOODEN_PICKAXE, 'C', Item.WOODEN_SHOVEL, 'Y', Item.STICK);
-            CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Stone_Paxel, 1), "ABC", " Y ", " Y ", 'A', Item.STONE_AXE, 'B', Item.STONE_PICKAXE, 'C', Item.STONE_SHOVEL, 'Y', Item.STICK);
-            CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Iron_Paxel, 1), "ABC", " Y ", " Y ", 'A', Item.IRON_AXE, 'B', Item.IRON_PICKAXE, 'C', Item.IRON_SHOVEL, 'Y', Item.STICK);
-            CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Gold_Paxel, 1), "ABC", " Y ", " Y ", 'A', Item.GOLDEN_AXE, 'B', Item.GOLDEN_PICKAXE, 'C', Item.GOLDEN_SHOVEL, 'Y', Item.STICK);
-            CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Diamond_Paxel, 1), "ABC", " Y ", " Y ", 'A', Item.DIAMOND_AXE, 'B', Item.DIAMOND_PICKAXE, 'C', Item.DIAMOND_SHOVEL, 'Y', Item.STICK);
+            CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Wood_Paxel, 1), "ABC", " Y ", " Y ", 'A', Item.WOODEN_AXE, 'B', Item.WOODEN_PICKAXE, 'C', Item.WOODEN_SHOVEL, 'Y', TagKey.of(ItemRegistry.KEY, Identifier.of("c:sticks/wooden")));
+            CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Stone_Paxel, 1), "ABC", " Y ", " Y ", 'A', Item.STONE_AXE, 'B', Item.STONE_PICKAXE, 'C', Item.STONE_SHOVEL, 'Y', TagKey.of(ItemRegistry.KEY, Identifier.of("c:sticks/wooden")));
+            CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Iron_Paxel, 1), "ABC", " Y ", " Y ", 'A', Item.IRON_AXE, 'B', Item.IRON_PICKAXE, 'C', Item.IRON_SHOVEL, 'Y', TagKey.of(ItemRegistry.KEY, Identifier.of("c:sticks/wooden")));
+            CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Gold_Paxel, 1), "ABC", " Y ", " Y ", 'A', Item.GOLDEN_AXE, 'B', Item.GOLDEN_PICKAXE, 'C', Item.GOLDEN_SHOVEL, 'Y', TagKey.of(ItemRegistry.KEY, Identifier.of("c:sticks/wooden")));
+            CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Diamond_Paxel, 1), "ABC", " Y ", " Y ", 'A', Item.DIAMOND_AXE, 'B', Item.DIAMOND_PICKAXE, 'C', Item.DIAMOND_SHOVEL, 'Y', TagKey.of(ItemRegistry.KEY, Identifier.of("c:sticks/wooden")));
             if (FabricLoader.getInstance().isModLoaded("aether")) {
                 CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Skyroot_Paxel, 1), "ABC", " Y ", " Y ", 'A', AetherItems.AxeSkyroot, 'B', AetherItems.PickSkyroot, 'C', AetherItems.ShovelSkyroot, 'Y', AetherItems.Stick);
                 CraftingRegistry.addShapedRecipe(new ItemStack(ItemListener.Holystone_Paxel, 1), "ABC", " Y ", " Y ", 'A', AetherItems.AxeHolystone, 'B', AetherItems.PickHolystone, 'C', AetherItems.ShovelHolystone, 'Y', AetherItems.Stick);
